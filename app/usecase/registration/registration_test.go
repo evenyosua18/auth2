378255dc13980bd7b51f55f0f8a65dd2e92687db
evenyosua18 <evenyosua18@gmail.com>
2024-02-18 22:34:58 +0700

    registration usecase creation unit testing

---
+++ app/usecase/registration/registration_test.go
@@ -0,0 +1,29 @@
+package registration
+
+import (
+	"github.com/evenyosua18/auth2/app/mock/repository/authdb"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestRegistration_Creation(t *testing.T) {
+	Convey("Test Registration Usecase Creation", t, func() {
+		Convey("creation should return valid struct", func() {
+			// mock repository
+			userRepo := &authdb.MUserRepository{}
+			accessTokenRepo := &authdb.MAccessTokenRepository{}
+			refreshTokenRepo := &authdb.MRefreshTokenRepository{}
+
+			res := NewRegistrationUsecase(userRepo, accessTokenRepo, refreshTokenRepo)
+
+			// response should be valid
+			Convey("validate response should be valid", func() {
+				So(res, ShouldEqual, &UsecaseRegistration{
+					user:         userRepo,
+					accessToken:  accessTokenRepo,
+					refreshToken: refreshTokenRepo,
+				})
+			})
+		})
+	})
+}
